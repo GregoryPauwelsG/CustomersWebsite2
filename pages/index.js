bb3a9464336d382e3ecbb3edeb466a712e6eff82
GregoryPauwelsG <97838451+GregoryPauwelsG@users.noreply.github.com>
2022-09-13 15:20:40 +0200

    changed p

--- pages/index.js
+++ pages/index.js
@@ -13,7 +13,7 @@ export default function Home() {
       <main>
         <Header title="Customer's Webiste 2" />
         <p>Website deployed to Netlify. Netlify provides a development platform for web applications and dynamic websites </p>
-        <p>React app is loaded on this page showing objects with a Category equal to 'Verwerker' </p>
+        <p>React app is loaded on this page showing the first 5 objects with a Category equal to 'Verwerker' </p>
         <embed type="text/html" src="https://testreactapprouter.cfapps.us10.hana.ondemand.com/#/tickets/Verwerker" width="700" height="400"></embed>
 
       </main>
