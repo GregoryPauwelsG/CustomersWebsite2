a99795565b2d7576831c4f154cabf3931ad9e788
GregoryPauwelsG <97838451+GregoryPauwelsG@users.noreply.github.com>
2022-09-19 09:27:53 +0200

    Adding a header

--- pages/index.js
+++ pages/index.js
@@ -12,6 +12,7 @@ export default function Home() {
 
       <main>
         <Header title="Customer's Webiste 2" />
+         <h1 className='display-4'>Test Header</h1>
         <p>Website deployed to Netlify. </p>
         <p>React app is loaded on this page showing the first 5 objects with a Category equal to 'Verwerker' </p>
         <embed type="text/html" src="https://testreactapprouter.cfapps.us10.hana.ondemand.com/#/tickets/Verwerker" width="700" height="400"></embed>
